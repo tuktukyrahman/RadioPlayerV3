b793758e88757e053c3e7770b91d011592ef38fe
TᑌKTᑌKY ᖇᗩᕼᗰᗩᑎ <90580448+tuktukyrahman@users.noreply.github.com>
2022-05-14 13:09:55 +0600

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def stop_and_restart():
 
 bot.run(main())
 bot.start()
-print("\n\nRadio Player Bot Started, Join @AsmSafone!")
+print("\n\nRadio Player Bot Started, Contact @P_R_I_Y_O_O")
 bot.send(
     SetBotCommands(
         scope=BotCommandScopeDefault(),
@@ -182,11 +182,11 @@ async def restart(_, message: Message):
             target=stop_and_restart()
             ).start()
     try:
-        await k.edit("✅ **Restarted Successfully! \nJoin @AsmSafone For Update!**")
+        await k.edit("✅ **Restarted Successfully! \nContact @P_R_I_Y_O_O For Update!**")
         await k.reply_to_message.delete()
     except:
         pass
 
 idle()
-print("\n\nRadio Player Bot Stopped, Join @AsmSafone!")
+print("\n\nRadio Player Bot Stopped, Contact @P_R_I_Y_O_O!")
 bot.stop()
